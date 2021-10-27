f96a119ccc272f0c16c528ac87e931f401ab5cf0
Martin Rodriguez Caraballo <64707521+mfrodriguezc@users.noreply.github.com>
2021-10-27 17:22:04 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+angular.module("carrito",[])
+    .controller("Desarrollo",function(){
+        var global=this;
+
+
+
+    });
+
+angular.module("main",[])
+    .controller("Principal",function(){
+
+
+        
+    });
